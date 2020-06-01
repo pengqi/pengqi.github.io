8302fb1c9d1c037017f4c492fc1af71d5ebbf03f
pengqi <wumingshi.sg@gmail.com>
2020-06-01 18:32:01 +0800

    Site updated: 2020-06-01 18:31:59

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,24 +1,3 @@
-var a_idx = 0;
-jQuery(document).ready(function($) {
-  $("body").click(function(e) {
-    var a = new Array("喜欢我", "不喜欢我");
-    var $i = $("<span/>").text(a[a_idx]);
-    var x = e.pageX,
-    y = e.pageY;
-    $i.css({
-      "z-index": 99999,
-      "top": y - 28,
-      "left": x - a[a_idx].length * 8,
-      "position": "absolute",
-      "color": "#ff7a45"
-    });
-    $("body").append($i);
-    $i.animate({
-      "top": y - 180,
-      "opacity": 0
-    }, 1500, function() {
-      $i.remove();
-    });
-    a_idx = (a_idx + 1) % a.length;
-  });
-});
+// build time:Mon Jun 01 2020 18:31:50 GMT+0800 (GMT+08:00)
+var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(e){var n=new Array("喜欢我","不喜欢我");var o=a("<span/>").text(n[a_idx]);var t=e.pageX,i=e.pageY;o.css({"z-index":99999,top:i-28,left:t-n[a_idx].length*8,position:"absolute",color:"#ff7a45"});a("body").append(o);o.animate({top:i-180,opacity:0},1500,function(){o.remove()});a_idx=(a_idx+1)%n.length})});
+//rebuild by neat 
